6968ec8e0d2ff2953ee8b1ebad23d8ef504984b4
Jeffrey <golettoo@gmail.com>
2012-07-04 13:05:24 +0800

    update log format

--- logUtil.py
+++ logUtil.py
@@ -7,6 +7,6 @@ log.setLevel(logging.DEBUG)
 if not len(log.handlers):
     handler = logging.FileHandler(filename='log.txt')
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter(fmt='%(asctime)s [%(levelname)s]: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    formatter = logging.Formatter(fmt='%(asctime)s [%(levelname)s] %(filename)s %(lineno)d %(funcName)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     handler.setFormatter(formatter)
     log.addHandler(handler)
