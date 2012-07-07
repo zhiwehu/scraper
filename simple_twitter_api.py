bcfcb5385611e714f4e385d1c63561ceca7505cf
Jeffrey <golettoo@gmail.com>
2012-07-07 11:37:33 +0800

    remove unused print

--- simple_twitter_api.py
+++ simple_twitter_api.py
@@ -7,7 +7,7 @@ from logUtil import log
 
 def UsersLookup(twitter_ids):
     url = 'https://api.twitter.com/1/users/lookup.json?screen_name=%s' % twitter_ids
-    log.debug(url)
+    #log.debug(url)
     f = urllib2.urlopen(url)
     json = f.read()
     f.close()
